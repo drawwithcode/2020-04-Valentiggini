f9b4ba2de4dc27dc000f9cad7d01f2a759b0d735
valentiggini <pippia.valentina@gmail.com>
2020-11-12 02:12:11 +0100

    ops2

--- sketch.js
+++ sketch.js
@@ -5,20 +5,39 @@ let letters = " .,;xe$";  // palette caratteri
 let charW = 6;            // larghezza cella caratteri
 let charH = 9;            // altezza cella caratteri
 let toneDiv = 100.0 / letters.length;
-let backgroundColor = 255
-let textColor = 0
-let darkTheme = false
+let backgroundColor = 255;
+let textColor = 0;
+let darkTheme = false;
+let changeWords = false;
+//let changeNumeri = false;
 
 function preload() {
-  photo = loadImage("assets/topolino.jpg");
+  photo = loadImage("assets/wallpaper.jpg");
   font = loadFont("assets/inconsolata.ttf");
 }
 
 function setup() {
-  createCanvas(1200, 720);
+  canvas = createCanvas(windowWidth, 655);
+  canvas.parent("sketch-div");
+
+  //button1
+
   button = createButton('Dark Theme')
-  button.mouseClicked(onClick)
+  button.class("buttonstyle");
+  button.mouseClicked(onClick);
   photo.resize(photo.width / charW, photo.height / charH);
+
+//button2
+
+  button2 = createButton('Change words');
+  button2.class("buttonstyle");
+  button2.mouseClicked(cambioLettere);
+
+//button3
+
+//  button3 = createButton('Numbers');
+//  button3.class("buttonstyle");
+//  button3.mouseClicked(numeri);
 
   drawAscii()
 }
@@ -40,15 +59,41 @@ function drawAscii() {
 
 function onClick() {
   if (!darkTheme) {
-    backgroundColor = 0
-    textColor = 255
-    button.html('Light Theme')
-    darkTheme = true
+    backgroundColor = 0;
+    textColor = 255;
+    button.html('Light Theme');
+    darkTheme = true;
   } else {
-    backgroundColor = 255
-    textColor = 0
-    button.html('Dark Theme')
-    darkTheme = false
+    backgroundColor = 255;
+    textColor = 0;
+    button.html('Dark Theme');
+    darkTheme = false;
   }
   drawAscii()
 }
+
+function cambioLettere() {
+  if (!changeWords) {
+    button2.html('Change words');
+    letters = " .,;xe$";
+    changeWords = true;
+
+  } else {
+    button2.html('reset');
+    letters = " -+°vYQ";
+    changeWords = false;
+  }
+  drawAscii()
+}
+
+//function numeri() {
+//if (!changeNumeri) {
+//button3.html('Numbers');
+//letters = " .014689";
+//changeNumeri = true;
+//} else {
+//  button3.html('reset');
+//  letters =" .,;xe$"
+//  changeNumeri = false;
+//}
+//}
